440f501addb7c949ac299a16540970632c4ca3ea
scole2 <scole2@GIX-30258897.netid.washington.edu>
2024-04-29 23:45:19 -0700

    add time repub node to get d435 timestamps to sim time

---
+++ realsense2_camera/scripts/time_republisher.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import PointCloud2
+from std_msgs.msg import Header
+
+class TimeRepublisher:
+    def __init__(self):
+        rospy.init_node('time_republisher', anonymous=True)
+        # Read parameters from ROS parameter server
+        image_topic = rospy.get_param('~image_topic', "/default")
+        print("Republishing image topic from: " + image_topic)
+        republished_image_topic = rospy.get_param('~republished_image_topic', "/default")
+        print("Republishing image topic to: " + republished_image_topic)
+        self.image_sub = rospy.Subscriber(image_topic, PointCloud2, self.image_callback)
+        self.image_pub = rospy.Publisher(republished_image_topic, PointCloud2, queue_size=10)
+
+    def image_callback(self, data):
+        # Get the current simulated time from /clock
+        current_time = rospy.get_rostime()
+        
+        # Update the header timestamp of the image message
+        data.header.stamp = current_time
+        
+        # Publish the image with simulated time
+        self.image_pub.publish(data)
+        print("s")
+
+if __name__ == '__main__':
+    
+    # Initialize the TimeRepublisher node
+    
+    tr = TimeRepublisher()
+    
+    rospy.spin()
